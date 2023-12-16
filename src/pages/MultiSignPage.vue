b22fd64721899bd776b1ac775cd97db0e4b1e2d1
Seth@Boid.com <mchosc@animusystems.com>
2023-12-16 15:57:19 +0000

    started to change how multisign is created in the ui

--- src/pages/MultiSignPage.vue
+++ src/pages/MultiSignPage.vue
@@ -5,25 +5,53 @@
         <div class="text-h6">
           Create a Multi Signature Proposal
         </div>
+        <q-card-section>
+          <div>
+            contract section
+          </div>
+          <!-- Separate fields for account and name -->
+          <q-input type="text" v-model="selectedAction.account" label="contract name" class="q-mt-md" />
+          <q-input type="text" v-model="selectedAction.name" label="action name" class="q-mt-md" />
+        </q-card-section>
+        <q-card-section>
+          <div>
+            authorization section
+          </div>
+          <!-- Dynamic authorization fields -->
+          <div class="q-mt-md">
+            <div v-for="(auth, index) in selectedAction.authorization" :key="index" class="row q-mb-md">
+              <q-input v-model="auth.actor" label="Actor" class="col" />
+              <q-input v-model="auth.permission" label="Permission" class="col" />
+              <q-btn icon="delete" color="negative" @click="removeAuthorization(index)" />
+            </div>
+            <q-btn icon="add" label="Add Authorization" @click="addAuthorization" />
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <div>
+            data section in JSON format
+          </div>
+          <!-- Text area for data as JSON -->
+          <q-input
+            type="textarea"
+            v-model="selectedActionDataJson"
+            label="Data (JSON)"
+            class="q-mt-md"
+            style="min-height: 300px"
+          />
+        </q-card-section>
+        <q-card-section>
+          <div>
+            required signers section
+          </div>
 
-        <!-- Text area for displaying and editing action data as JSON -->
-        <q-input
-          type="textarea"
-          v-model="selectedActionDataJson"
-          label="Action Data (JSON)"
-          class="q-mt-md"
-          style="min-height: 300px"
-        />
-
-        <!-- Text area for displaying and editing reqSignAccs data -->
-        <q-input
-          type="textarea"
-          v-model="reqSignAccsJson"
-          label="Required Signatory Accounts (JSON)"
-          class="q-mt-md"
-          style="min-height: 300px"
-        />
-
+          <div v-for="(item, index) in reqSignAccs" :key="index" class="row q-mb-md">
+            <q-input v-model="item.actor" label="Actor" class="col" />
+            <q-input v-model="item.permission" label="Permission" class="col" />
+            <q-btn icon="delete" color="negative" @click="removeReqSignAccs(index)" />
+          </div>
+          <q-btn icon="add" label="Add Actor/Permission" @click="addReqSignAccs" />
+        </q-card-section>
         <q-card-actions align="right">
           <q-btn flat label="Create" color="green" @click="createProposal" />
         </q-card-actions>
@@ -33,48 +61,94 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from "vue"
+import { onMounted, computed, reactive } from "vue"
 import { createAndExecuteMultiSignProposal } from "src/lib/contracts"
+import { Name } from "@wharfkit/antelope"
+import { Types as TypesMultiSign } from "src/lib/eosio-msig-contract-telos-mainnet"
 
-const selectedActionDataJson = ref("")
-const actions = [
-  {
-    account: "wt.boid",
-    name: "transfer",
-    data: { from: "vault.boid", to: "service.boid", quantity: "2000000.0000 BOID", memo: "for user rewards" },
-    authorization: [{ actor: "vault.boid", permission: "active" }]
+const selectedAction = reactive({
+  account: "",
+  name: "",
+  authorization: [{ actor: "", permission: "" }],
+  data: {}
+})
+
+const selectedActionDataJson = computed({
+  get: () => JSON.stringify(selectedAction.data, null, 2),
+  set: (value) => {
+    try {
+      selectedAction.data = JSON.parse(value)
+    } catch (error) {
+      console.error("Error parsing action data JSON:", error)
+    }
   }
-]
-const reqSignAccsJson = ref(JSON.stringify([
+})
+
+const addAuthorization = () => {
+  selectedAction.authorization.push({ actor: "", permission: "" })
+}
+
+const removeAuthorization = (index:number) => {
+  selectedAction.authorization.splice(index, 1)
+}
+
+const reqSignAccs = reactive([
   { actor: "boid.animus", permission: "active" },
-  { actor: "imjohnatboid", permission: "active" },
-  { actor: "ticitelos113", permission: "active" }
-], null, 2))
+  { actor: "imjohnatboid", permission: "active" }
+])
+
+const addReqSignAccs = () => {
+  reqSignAccs.push({ actor: "", permission: "" })
+}
+
+const removeReqSignAccs = (index:number) => {
+  reqSignAccs.splice(index, 1)
+}
 
 const createProposal = async() => {
-  console.log("Creating proposal with action:", selectedActionDataJson.value)
-
   try {
-    const parsedActionData = JSON.parse(selectedActionDataJson.value)
-    const parsedReqSignAccsData = JSON.parse(reqSignAccsJson.value)
+    // Convert the data JSON string back to an object
+    selectedAction.data = JSON.parse(selectedActionDataJson.value)
+
+    // Convert the authorizations to the required type
+    const authorizationsConverted = selectedAction.authorization.map(auth => ({
+      actor: Name.from(auth.actor),
+      permission: Name.from(auth.permission)
+    }))
+
+    // Construct the complete action object
+    // eslint-disable-next-line new-cap
+    const action = new TypesMultiSign.action({
+      account: selectedAction.account,
+      name: selectedAction.name,
+      authorization: authorizationsConverted,
+      data: selectedAction.data
+    })
 
     // Actions array for the proposal
-    const actionsForProposal = [parsedActionData]
-
-    console.log("Actions for proposal:", actionsForProposal)
+    const actionsForProposal = [action]
+    console.log("actionsForProposal:", actionsForProposal)
+    // Convert reqSignAccs to the required type for the proposal
+    const reqSignAccsConverted = reqSignAccs.map(item =>
+      // eslint-disable-next-line new-cap
+      new TypesMultiSign.permission_level({
+        actor: Name.from(item.actor),
+        permission: Name.from(item.permission)
+      })
+    )
+    console.log("reqSignAccsConverted:", reqSignAccsConverted)
 
     // Attempt to create the proposal with the actions
-    const result = await createAndExecuteMultiSignProposal(parsedReqSignAccsData, actionsForProposal)
+    const result = await createAndExecuteMultiSignProposal(reqSignAccsConverted, actionsForProposal)
     console.log("Proposal created:", result)
   } catch (error) {
     console.error("Error creating proposal:", error)
   }
 }
 
+
 onMounted(() => {
-  if (actions.length > 0) {
-    selectedActionDataJson.value = JSON.stringify(actions[0], null, 2)
-  }
+
 })
 
 </script>
